b297b874d22635e92bb172b654448ad6427a5420
YunYuLin <alina001212@gmail.com>
2025-09-15 00:01:20 +0200

    Finish Q3: 8-bit unsigned CSA and testbench

--- q3/csa_8.sv
+++ q3/csa_8.sv
@@ -2,8 +2,29 @@ module csa_8 (
   input logic [7:0] a, b,
   output logic [7:0] sum,
   output logic carry
+
 );
   // …
   // Add your description here
+
+  logic c3;      // carry of low adder to use as 2to1_MUX select
+  logic c0, c1;  // carry of two high adder
+  logic [3:0] sum0, sum1;
+  logic c_inc;   // carry of the high a + high b + 1
+
+  // low 4 bit
+  adder_4 lo_add ( .A( a[3:0] ), .B( b[3:0] ), .sum( sum[3:0] ), .carry( c3 ) );
+
+  // high 4 bit, two case
+  // ci = 0
+  adder_4 hi0_add ( .A( a[7:4] ), .B( b[7:4] ), .sum( sum0[3:0] ), .carry( c0 ) );
+  // ci = 1, hi0_add + 1 
+  adder_4 hi1_add ( .A( sum0[3:0] ), .B( 4'b0001 ), .sum( sum1[3:0] ), .carry( c_inc ) );
+  assign c1 = c0 | c_inc;
+
+  // select
+  assign sum[7:4] = c3 ? sum1[3:0] : sum0[3:0];
+  assign carry = c3 ? c1 : c0;
+
   // …
 endmodule
--- q3/csa_8_tb.sv
+++ q3/csa_8_tb.sv
@@ -3,9 +3,52 @@ module csa_8_tb;
   // Inputs
   logic [7:0] a;
   logic [7:0] b;
-  logic [7:0] sum,
-  logic carry;
+  logic [7:0] sum;
+  logic       carry;
+
+  // check
+  logic [8:0] ans;
 
   // complete
-  
+  csa_8 uut(
+    .a    (a),
+    .b    (b),
+    .sum  (sum),
+    .carry(carry)  );
+
+  initial begin
+    $display(" time  |   a   +   b   | carry  sum  |  ANS");
+
+    // given 7 case
+    a = 8'd0;    b = 8'd0;    #1 check_result; // 0, c7 = 0
+    a = 8'd2;    b = 8'd3;    #1 check_result; // c3 = 0, c0 = 0, c_inc = 0, c7 = 0
+    a = 8'd240;  b = 8'd16;   #1 check_result; // c3 = 0, c0 = 1, c_inc = 0, c7 = 1
+    a = 8'd240;  b = 8'd0;    #1 check_result; // c3 = 0, c0 = 0, c_inc = 1, c7 = 0
+    a = 8'd15;   b = 8'd17;   #1 check_result; // c3 = 1, c0 = 0, c_inc = 0, c7 = 0
+    a = 8'd255;  b = 8'd17;   #1 check_result; // c3 = 1, c0 = 1, c_inc = 0, c7 = 1
+    a = 8'd255;  b = 8'd1;    #1 check_result; // c3 = 1, c0 = 0, c_inc = 1, c7 = 1
+
+    // random
+    repeat (5) begin
+      a = $urandom_range(0, 255);
+      b = $urandom_range(0, 255);
+      #5 check_result;
+    end
+
+    $display("All tests finished.");
+    $stop;
+  end
+
+
+  task check_result;
+    ans = a + b;
+    if ( {carry, sum} !== ans ) begin
+      $display("[%0t] ERROR: %0d, %0d => %0b, %0d, should be=%0b %0d",
+               $time, a, b, carry, sum, ans[8], ans[7:0]);
+    end else begin
+      $display("[%0t] PASS : a=%0d b=%0d => %0b, %0d",
+               $time, a, b, carry, sum);
+    end
+  endtask
+
 endmodule
